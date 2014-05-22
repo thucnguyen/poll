3dbdb751b6f8feca6798e219aa1d518b93a6ecc4
Thuc <thucnguyen>
2014-05-22 15:01:30 +0700

    Added rails_12factor gem and updated Gemfile.lock

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,11 @@ class Question < ActiveRecord::Base
   
   # asociation
   has_many :polls
+  
+  accepts_nested_attributes_for :polls
+  
+  # def polls_for_form
+    # collection = polls.where(question_set_id: id)
+    # collection.any? ? collection : polls.build
+  # end
 end
